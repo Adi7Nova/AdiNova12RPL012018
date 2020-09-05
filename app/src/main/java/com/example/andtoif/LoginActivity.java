2820318c392420d28247a74ee9a97d972de7f1af
Adi7Nova <adixrpl2018smkrus@gmail.com>
2020-09-05 18:56:00 +0700

    v1

---
+++ app/src/main/java/com/example/andtoif/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.example.andtoif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.andtoif.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String ID = "id";
+    public static final String USERNAME = "username";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Button login = findViewById(R.id.btnlogin);
+        final EditText txtusername = findViewById(R.id.username);
+        final EditText txtpass = findViewById(R.id.password);
+        final TextView regis = findViewById(R.id.register);
+
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(in);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = txtusername.getText().toString();
+                final String password = txtpass.getText().toString();
+                AndroidNetworking.post("http://192.168.1.8/API/login.php")
+                        .addBodyParameter("nama", txtusername.getText().toString() )
+                        .addBodyParameter("password", txtpass.getText().toString() )
+                        .setTag("test")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("cek response", String.valueOf(response));
+                                if ((username.isEmpty() || password.isEmpty())) {
+                                    Toast.makeText(LoginActivity.this, "semuanya harus di isi", Toast.LENGTH_SHORT).show();
+                                } else {
+
+                                    try {
+                                        int suksess = response.getInt("success");
+                                        int id = response.getInt("id");
+                                        if (suksess == 1 ){
+                                            Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
+                                            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                                            editor.putString(ID, String.valueOf(id));
+                                            editor.putString(USERNAME, String.valueOf(username));
+                                            editor.apply();
+                                            Intent in = new Intent(LoginActivity.this, Dashboard.class);
+                                            startActivity(in);
+                                            finish();
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+
+                            }
+                            @Override
+                            public void onError(ANError error) {
+                                Toast.makeText(LoginActivity.this, "gagal",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/andtoif/RegisterActivity.java
@@ -0,0 +1,94 @@
+package com.example.andtoif;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.andtoif.R;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ProgressDialog pDialog;
+    Intent intent;
+    HashMap<String, String> hashMap = new HashMap<>();
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Assign Id'S
+        Button register = findViewById(R.id.btnregister);
+        TextView login = findViewById(R.id.readyaccount);
+        final EditText txt_username = (EditText) findViewById(R.id.txtusername1);
+        final EditText txt_password = (EditText) findViewById(R.id.txtpass1);
+        final EditText nohp = findViewById(R.id.txtnomerhp);
+        final EditText alamat = findViewById(R.id.txtalamat);
+        final EditText email = findViewById(R.id.txtemail);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AndroidNetworking.post("http://192.168.1.8/API/register.php")
+                        .addBodyParameter("username", txt_username.getText().toString())
+                        .addBodyParameter("nohp", nohp.getText().toString())
+                        .addBodyParameter("email", email.getText().toString())
+                        .addBodyParameter("alamat", alamat.getText().toString())
+                        .addBodyParameter("password", txt_password.getText().toString())
+                        .setTag("test")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                String username = txt_username.getText().toString();
+                                String password = txt_password.getText().toString();
+                                String alamatt = alamat.getText().toString();
+                                String emaill = email.getText().toString();
+                                String nohpp = nohp.getText().toString();
+
+                                if ((username.isEmpty()) || (password.isEmpty()) || (alamatt.isEmpty()) || (emaill.isEmpty() || (nohpp.isEmpty()))) {
+                                    Toast.makeText(RegisterActivity.this, "semuanya harus di isi", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "success", Toast.LENGTH_SHORT).show();
+                                    Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(in);
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError error) {
+                                Toast.makeText(RegisterActivity.this, "gagal", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(in);
+            }
+        });
+
+
+    }
+
+
+}
